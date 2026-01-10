46907c8026d88b0f726ed41122c79a68bd276b86
sulusgit <116257@student.uthrad.pl>
2026-01-10 11:05:19 +0100

    ne

--- inc/db.php
+++ inc/db.php
@@ -1,5 +1,5 @@
 <?php
-//writning func to  DB
+//writning func to  DB vfjdkbgnkdfb gds dfvbgkdzsjvsznzgodb
 @$con = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 if (!$con) {
     $error = mysqli_connect_errno();
